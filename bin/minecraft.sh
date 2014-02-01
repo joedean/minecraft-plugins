78832f1146a98d7e0fce8b07fc221f03c5855f02
Joseph Dean <joe@on-site.com>
2014-01-31 21:53:56 -0800

    cleaned conditional logic in shell script and start jruby from jar

--- bin/minecraft.sh
+++ bin/minecraft.sh
@@ -7,25 +7,25 @@ ACCEPTABLE_JAVA_VERSION=6
 JAVA_VERSION=`java -version 2>&1 | head -1 | awk -F '"' '{print $2}'`
 
 if [ ${JAVA_VERSION} = "" ]; then
-  echo Please install Java
-else
-  JAVA_MAJOR_VERSION=`echo ${JAVA_VERSION} | cut -d '.' -f 2`
+  echo Please install Java or configure PATH correctly
+  exit 1
 fi
+JAVA_MAJOR_VERSION=`echo ${JAVA_VERSION} | cut -d '.' -f 2`
 
 if [ ${JAVA_MAJOR_VERSION} -lt ${ACCEPTABLE_JAVA_VERSION} ]; then
   echo You need to upgrade Java
-else
-  # check if wget is installed, if not install.
-  if [ -d ~/.coderdojo ]; then
-    cd ~/.coderdojo
-  else
-    cd ~ && mkdir .coderdojo && cd .coderdojo
-  fi
-
-  if [ -f ~/.coderdojo/jruby-complete.jar ]; then
-    echo good job you have jruby!
-  else
-    wget http://jruby.org.s3.amazonaws.com/downloads/1.7.10/jruby-complete-1.7.10.jar
-    mv jruby-complete-1.7.10.jar jruby-complete.jar
-  fi
+  exit 1
 fi
+
+# check if wget is installed, if not install.
+if [ ! -d ~/.coderdojo ]; then
+  mkdir ~/.coderdojo
+fi
+
+cd ~/.coderdojo
+if [ ! -f ~/.coderdojo/jruby-complete.jar ]; then
+  wget http://jruby.org.s3.amazonaws.com/downloads/1.7.10/jruby-complete-1.7.10.jar
+  mv jruby-complete-1.7.10.jar jruby-complete.jar
+fi
+
+java -Xmx500m -Xss1024k -jar ~/.coderdojo/jruby-complete.jar check_environment.rb
